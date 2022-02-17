751c6b94ccddf6d19e1b137881f5018a0e26b162
m-shimizu <62657297+m-shimizu-ring@users.noreply.github.com>
2022-02-17 23:49:26 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <DataTable />
+  </div>
+</template>
+
+<script>
+import DataTable from "./components/dataTable";
+
+export default {
+  name: "App",
+  components: {
+    DataTable,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/dataTable.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app>
+    <v-card class="ma-4">
+      <v-data-table :items="bookDate" :headers="headers" hide-default-footer>
+        <template v-slot:[`item.edit`]="{ item }">
+          <v-btn small color="success"> 編集 </v-btn>
+        </template>
+      </v-data-table>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "ID",
+          value: "id",
+        },
+        { text: "タイトル", value: "title" },
+        { text: "内容", value: "content" },
+        {
+          text: "",
+          value: "edit",
+          sortable: false,
+        },
+      ],
+      bookDate: [
+        {
+          id: 1,
+          title: "ナルト",
+          content: "面白い",
+        },
+        {
+          id: 2,
+          title: "鋼の錬金術師",
+          content: "面白い",
+        },
+        {
+          id: 3,
+          title: "ワールドトリガー",
+          content: "面白い",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
