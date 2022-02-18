f48fb2664150219e248f190aabaddbe4b540c064
m-shimizu <62657297+m-shimizu-ring@users.noreply.github.com>
2022-02-18 23:15:42 +0900

    モーダル実装

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -1,10 +1,55 @@
 <template>
-  <v-btn small color="success"> 編集 </v-btn>
+  <div>
+    <v-btn small color="success" @click="openModal"> 編集 </v-btn>
+    <div id="overlay" v-show="showContent">
+      <div id="content">
+        <p>これがモーダルウィンドウです。</p>
+        <button @click="closeModal">Close</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      showContent: false,
+    };
+  },
+  methods: {
+    openModal: function () {
+      this.showContent = true;
+    },
+    closeModal: function () {
+      this.showContent = false;
+    },
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+#overlay {
+  /*　要素を重ねた時の順番　*/
+  z-index: 1;
+
+  /*　画面全体を覆う設定　*/
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+
+  /*　画面の中央に要素を表示させる設定　*/
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+#content {
+  z-index: 2;
+  width: 50%;
+  padding: 1em;
+  background: #fff;
+}
 </style>
